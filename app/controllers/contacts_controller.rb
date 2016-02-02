10abbd69343acd885869ab3d559d971fc5a90c80
JARED ARKIN <ARKINJB@GMAIL.COM>
2016-02-01 21:52:09 -0500

    added bootstrap gem, new/edit form modals

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,12 +8,34 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
+  def create
+    @contact = Contact.create!(contact_params)
+    redirect_to (contact_path(@contact))
+  end
+
   def show
     @contact = Contact.find(params[:id])
   end
 
   def edit
     @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.update(contact_params)
+    redirect_to contact_path(@contact)
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to contacts_path
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :address, :city, :state, :zip_code)
   end
 
 end
